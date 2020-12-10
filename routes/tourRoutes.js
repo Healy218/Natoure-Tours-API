d4057e8888c5cdefe436be4adc21cd7692d2b52e
healy218 <mrhealy115@gmail.com>
2020-12-09 20:07:31 -0600

    added async

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
+
 const router = express.Router();
 
-router.param('id', tourController.checkID);
+// router.param('id', tourController.checkID);
 
 //Create a checkBody middleware
 //Check if body contains price and name property
@@ -11,7 +12,7 @@ router.param('id', tourController.checkID);
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.checkBody, tourController.createTour);
+  .post(tourController.createTour);
 
 router
   .route('/:id')
