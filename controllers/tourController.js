9baf017008207e3bd811a44cfc3c7d2dc8ee179d
healy218 <mrhealy115@gmail.com>
2020-12-14 10:43:35 -0600

    Added query strings to tour controller

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,7 +2,11 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    const tours = await Tour.find()
+      .where('duration')
+      .equals(5)
+      .where('difficulty')
+      .equals('easy');
 
     res.status(200).json({
       status: 'success',
