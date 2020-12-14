123c7f048d9eea8674d6b7a4f5352961d32559ad
healy218 <mrhealy115@gmail.com>
2020-12-14 14:15:28 -0600

    Added queries, sorting and field limiting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,12 +2,46 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find()
-      .where('duration')
-      .equals(5)
-      .where('difficulty')
-      .equals('easy');
+    //BUILD QUERY
+    //1A) Filtering
+    const queryObj = { ...req.query };
+    const excludeFeilds = ['page', 'sort', 'limit', 'fields'];
+    excludeFeilds.forEach((el) => delete queryObj[el]);
 
+    //1B) Advanced Filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    console.log(JSON.parse(queryStr));
+
+    let query = Tour.find(JSON.parse(queryStr));
+
+    //2) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      console.log(sortBy);
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    //3) Feild Limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    // const tours = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+    //EXECUTE QUERY
+    const tours = await query;
+
+    //SEND RESPONSE
     res.status(200).json({
       status: 'success',
       requestedAt: req.requestTime,
