99e1c9e28a8b7b463a9a67803844e8531c05d7ef
healy218 <mrhealy115@gmail.com>
2020-12-23 17:14:38 -0600

    Added simple global error middleware

--- app.js
+++ app.js
@@ -30,4 +30,14 @@ app.all('*', (req, res, next) => {
   });
 });
 
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
+
 module.exports = app;
