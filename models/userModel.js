55da27b30f6bca7b2b29e2f0ba8df509391797da
healy218 <mrhealy115@gmail.com>
2021-01-05 11:48:52 -0600

    imported npm bcrypt and hashed passwords

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-
+const bcrypt = require('bcryptjs');
 //name, email, photo, password, passwordconfirm
 
 const userSchema = new mongoose.Schema({
@@ -21,17 +21,33 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: [true, 'User needs a password'],
-    maxlength: [40, 'A tour name must have less than 40 characters'],
-    minlength: [10, 'A tour name must have more than 10 characters'],
+    minlength: [8, 'Password must be at least 8 characters'],
     // validate: [validator.isAlpha, 'Tour name must only contain characters'],
   },
   passwordConfirm: {
     type: String,
     required: [true, 'Passwords must match'],
-    maxlength: [40, 'A tour name must have less than 40 characters'],
-    minlength: [10, 'A tour name must have more than 10 characters'],
-    // validate: [validator.isAlpha, 'Tour name must only contain characters'],
+    minlength: [8, 'A tour name must have more than 10 characters'],
+    validate: {
+      // This only works on Save!
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'passwords are not the same!',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  //Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  //Delete the password confirmed field
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
